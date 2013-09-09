cf4abe80e73e636f3e30dd525cef1c4624229af9
Zdybai Dmytro <zdybai93@gmail.com>
2013-09-09 22:54:40 +0300

    Stack with tests

---
+++ src/com/epampractice/lab0/Stack.java
@@ -0,0 +1,11 @@
+package com.epampractice.lab0;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: JavaCoder
+ * Date: 08.09.13
+ * Time: 1:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class Stack {
+}
---
+++ tests/com/epampractice/lab0/StackTest.java
@@ -0,0 +1,28 @@
+package com.epampractice.lab0;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: JavaCoder
+ * Date: 08.09.13
+ * Time: 4:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class StackTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testPush() throws Exception {
+
+    }
+
+    @Test
+    public void testPop() throws Exception {
+
+    }
+}
